3be02a0b15731e6a1a33a6fcac48ed77b0258dca
Crno0 <123456789+odin@users.noreply.github.com>
2024-04-21 18:40:38 +0800

    Create form route and post

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,32 @@ import messages from '../models/db/messages.js';
 
 const router = Router();
 
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+
 /* GET home page. */
 router.get('/', (req, res, _) => {
-    res.render('index', { messages, title: 'Message board'});
+    res.render('index', { messages, options, title: 'Message board' });
+});
+
+// GET form
+router.get('/new', (req, res, _) => {
+    res.render('form', { title: 'form' });
+});
+
+// Create new message
+router.post('/new', (req, res) => {
+    messages.push({
+        user: req.body.user,
+        text: req.body.message,
+        added: new Date(),
+    });
+
+    res.redirect('/');
 });
 
 export default router;
