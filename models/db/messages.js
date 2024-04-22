5bd2a03d446dac131305338e09d44f44724110fc
Crno0 <123456789+odin@users.noreply.github.com>
2024-04-22 16:17:57 +0800

    Add dummy user

--- models/db/messages.js
+++ models/db/messages.js
@@ -8,7 +8,13 @@ const messages = [
       text: "Hello World!",
       user: "Charles",
       added: new Date()
-    }
+    },
+    {
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem harum asperiores laudantium dolore est quo quia animi expedita pariatur voluptatum eligendi cum, ipsum commodi officia repudiandae? Labore earum veritatis inventore?',
+      user: 'Crno',
+      added: new Date(),
+    },
+    
  ];
 
 export default messages;
